aed05d9c71d0cfb53c08ff31db66d6ff3f908649
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2017-04-22 19:01:14 +0200

    Working script runtime 40-60m

--- psconfeu2017/ALDeployment/AutomatedLab.ps1
+++ psconfeu2017/ALDeployment/AutomatedLab.ps1
@@ -12,7 +12,9 @@ Add-LabAzureSubscription -Path $azureContext -DefaultLocationName $defaultLocati
 Sync-LabAzureLabSources -SkipIsos -MaxFileSizeInMb 1
 
 # Define a domain
-Add-LabDomainDefinition -Name $domainName -AdminUser 'jhp' -AdminPassword 'Somepass1'
+Add-LabDomainDefinition -Name $domainName -AdminUser jhp -AdminPassword Somepass1
+
+Set-LabInstallationCredential -Username jhp -Password Somepass1
 
 $PSDefaultParameterValues = @{
     'Add-LabMachineDefinition:DomainName' = $domainName
@@ -20,17 +22,22 @@ $PSDefaultParameterValues = @{
 }
 
 # Add a DC with post-installation scripts for user creation
+
+$postInstallActivity = @()
+$postInstallActivity += Get-LabPostInstallationActivity -ScriptFileName PrepareRootDomain.ps1 -DependencyFolder $labSources\PostInstallationActivities\PrepareRootDomain
+$postInstallActivity += Get-LabPostInstallationActivity -ScriptFileName 'New-ADLabAccounts 2.0.ps1' -DependencyFolder $labSources\PostInstallationActivities\PrepareFirstChildDomain
+
 $roles = @()
 $roles += Get-LabMachineRoleDefinition -Role RootDC
 #$roles += Get-LabMachineRoleDefinition -Role CaRoot
-Add-LabMachineDefinition -Name 'jhp-dc01' -MinMemory 1GB -Roles $roles
+Add-LabMachineDefinition -Name 'jhp-dc01' -MinMemory 1GB -Roles $roles -AzureProperties @{RoleSize = "Standard_A2"}
 
 # Add a SQL 2014 server
 $role = Get-LabMachineRoleDefinition -Role SQLServer2014
-Add-LabMachineDefinition -Name 'jhp-sql01' -MinMemory 1GB  -Roles $role
+Add-LabMachineDefinition -Name 'jhp-sql01' -MinMemory 1GB  -Roles $role -AzureProperties @{RoleSize = "Standard_A2"}
 
 # Add a simple web server
-Add-LabMachineDefinition -Name 'jhp-web01' -Roles 'WebServer'
+Add-LabMachineDefinition -Name 'jhp-web01' -Roles 'WebServer' -AzureProperties @{RoleSize = "Standard_A2"}
 
 Install-Lab
 
