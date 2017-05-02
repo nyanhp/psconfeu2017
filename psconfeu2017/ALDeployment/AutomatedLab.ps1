ad14e5f9e930c0aeb6de900171d590883210732a
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2017-05-02 17:55:15 +0200

    Added pure powershell azure deployment

--- psconfeu2017/ALDeployment/AutomatedLab.ps1
+++ psconfeu2017/ALDeployment/AutomatedLab.ps1
@@ -1,7 +1,7 @@
 ﻿$labName = 'psconfeu2017jhp'
 $defaultLocation = 'west europe'
 $azureContext = 'D:\Jhp.azurermsettings'
-$domainName = 'poshjhp.com'
+$domainName = 'powershell.rules'
 
 # Lab definition
 New-LabDefinition -Name $labname -DefaultVirtualizationEngine Azure
@@ -12,16 +12,16 @@ Add-LabAzureSubscription -Path $azureContext -DefaultLocationName $defaultLocati
 Sync-LabAzureLabSources -SkipIsos -MaxFileSizeInMb 1
 
 # Define a domain
-Add-LabDomainDefinition -Name $domainName -AdminUser jhp -AdminPassword Somepass1
+Add-LabDomainDefinition -Name $domainName -AdminUser posh -AdminPassword Somepass1
 
-Set-LabInstallationCredential -Username jhp -Password Somepass1
+Set-LabInstallationCredential -Username posh -Password Somepass1
 
 $PSDefaultParameterValues = @{
     'Add-LabMachineDefinition:DomainName' = $domainName
     'Add-LabMachineDefinition:OperatingSystem' = 'Windows Server 2012 R2 SERVERDATACENTER'
 }
 
-# Add a DC with post-installation scripts for user creation
+# Add a DC with post-installation scripts for user creation including a CA
 
 $postInstallActivity = @()
 $postInstallActivity += Get-LabPostInstallationActivity -ScriptFileName PrepareRootDomain.ps1 -DependencyFolder $labSources\PostInstallationActivities\PrepareRootDomain
@@ -29,14 +29,14 @@ $postInstallActivity += Get-LabPostInstallationActivity -ScriptFileName 'New-ADL
 
 $roles = @()
 $roles += Get-LabMachineRoleDefinition -Role RootDC
-#$roles += Get-LabMachineRoleDefinition -Role CaRoot
+$roles += Get-LabMachineRoleDefinition -Role CaRoot
 Add-LabMachineDefinition -Name 'poshconf-dc01' -MinMemory 1GB -Roles $roles -AzureProperties @{RoleSize = "Standard_D2"}
 
-# Add a SQL 2014 server
+# Add a domain-joined SQL 2014 server
 $role = Get-LabMachineRoleDefinition -Role SQLServer2014
 Add-LabMachineDefinition -Name 'poshconf-sql01' -MinMemory 1GB  -Roles $role -AzureProperties @{RoleSize = "Standard_D2"}
 
-# Add a simple web server
+# Add a domain-joined simple web server
 Add-LabMachineDefinition -Name 'poshconf-web01' -Roles 'WebServer' -AzureProperties @{RoleSize = "Standard_D2"}
 
 Install-Lab
---
+++ psconfeu2017/psconfeu2017_pureShell/PowerShellDeployment.ps1
@@ -0,0 +1,91 @@
+﻿[string]$SubscriptionPath = 'D:\jhp.azurermsettings'
+[string]$SubscriptionName = 'AL4'
+[string]$ResourceGroupName = "Test3"
+[string]$Location = 'West Europe'
+[string]$LabName = 'Test3'
+[string]$PublisherName = 'MicrosoftWindowsServer'
+[string]$OfferName = 'WindowsServer'
+[string]$SkusName = '2016-Datacenter'  
+
+Select-AzureRmProfile -Path $SubscriptionPath
+Set-AzureRmContext -SubscriptionName $SubscriptionName
+
+
+New-AzureRmResourceGroup -Name $ResourceGroupName -ErrorAction SilentlyContinue -Location $Location -Force
+New-AzureRmStorageAccount -ResourceGroupName $ResourceGroupName -Name automatedlabmfaxwkjw -ErrorAction SilentlyContinue -Location $Location -SkuName Standard_LRS
+[object]$StorageContext = (Get-AzureRmStorageAccount -ResourceGroupName $ResourceGroupName -Name automatedlabmfaxwkjw).Context
+
+$subnetConfig = New-AzureRmVirtualNetworkSubnetConfig -Name Subnet1 -AddressPrefix '192.168.4.0/24'
+New-AzureRmVirtualNetwork -ResourceGroupName $ResourceGroupName -Name $LabName -Location $Location -AddressPrefix '192.168.4.0/24' -Force -Subnet $subnetConfig
+
+
+$Machines = @(
+        @{
+            Name = 'DC'
+            IpV4Address = '192.168.4.7'
+            Network = '192.168.4.0/24'
+        }
+        @{
+            Name = 'WEB1'
+            IpV4Address = '192.168.4.8'
+            Network = '192.168.4.0/24'
+        }
+        @{
+            Name = 'WEB2'
+            IpV4Address = '192.168.4.79'
+            Network = '192.168.4.0/24'
+        }
+)
+
+foreach($machine in $Machines)
+{
+
+[object]$Disks = @()
+[string]$Vnet = 'Test3'
+[string]$RoleSize = 'Standard_D2'
+[string]$AdminUserName = 'Install'
+[string]$AdminPassword = 'Somepass1'
+[string]$MachineResourceGroup = 'Test3'
+[object]$DefaultIpAddress =  $Machine.IPV4Address
+      
+
+
+$subnet = (Get-AzureRmVirtualNetwork -ResourceGroupName $ResourceGroupName |
+Where-Object { $_.AddressSpace.AddressPrefixes.Contains($Machine.Network) })[0] |
+Get-AzureRmVirtualNetworkSubnetConfig
+                                     
+$securePassword = ConvertTo-SecureString -String $AdminPassword -AsPlainText -Force
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($AdminUserName, $securePassword)
+
+$machineAvailabilitySet = Get-AzureRmAvailabilitySet -ResourceGroupName $ResourceGroupName -Name ($Machine.Network)[0] -ErrorAction SilentlyContinue
+if(-not ($machineAvailabilitySet))
+{
+    $machineAvailabilitySet = New-AzureRmAvailabilitySet -ResourceGroupName $ResourceGroupName -Name ($Machine.Network)[0] -Location $Location -ErrorAction Stop   
+}
+
+$vm = New-AzureRmVMConfig -VMName $Machine.Name -VMSize $RoleSize -ErrorAction Stop -AvailabilitySetId $machineAvailabilitySet.Id
+$vm = Set-AzureRmVMOperatingSystem -VM $vm -Windows -ComputerName $Machine.Name -Credential $cred -ProvisionVMAgent -EnableAutoUpdate -ErrorAction Stop -WinRMHttp
+$vm = Set-AzureRmVMSourceImage -VM $vm -PublisherName $PublisherName -Offer $OfferName -Skus $SkusName -Version "latest" -ErrorAction Stop
+$defaultIPv4Address = $DefaultIpAddress
+
+$nicProperties = @{
+    Name = "$($Machine.Name.ToLower())nic0"
+    ResourceGroupName = $ResourceGroupName
+    Location = $Location
+    Subnet = $subnet
+    PrivateIpAddress = $defaultIPv4Address
+    ErrorAction = "Stop"
+}
+        
+$networkInterface = New-AzureRmNetworkInterface @nicProperties
+        
+$vm = Add-AzureRmVMNetworkInterface -VM $vm -Id $networkInterface.Id -ErrorAction Stop
+        
+                                   
+$DiskName = "$($machine.Name)_os"
+$OSDiskUri = "$($StorageContext.BlobEndpoint)automatedlabdisks/$DiskName.vhd"
+        
+$vm = Set-AzureRmVMOSDisk -VM $vm -Name $DiskName -VhdUri $OSDiskUri -CreateOption fromImage -ErrorAction Stop
+
+New-AzureRmVM -ResourceGroupName $ResourceGroupName -Location $Location -VM $vm -ErrorAction Stop -Verbose
+}
